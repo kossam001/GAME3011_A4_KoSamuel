11ca6169223d295e71f008bd19701b4694113a8a
kossam001 <56322486+kossam001@users.noreply.github.com>
2021-04-09 10:28:52 -0400

    Board Generation

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Board : MonoBehaviour
 {
     private static Board instance;
     public static Board Instance { get { return instance; } }
+
+    public int rows;
+    public int columns;
+
+    public GameObject tilePrefab;
+    public Transform boardTransform;
+    public Tile[,] board;
 
     private void Awake()
     {
@@ -15,5 +23,26 @@ public class Board : MonoBehaviour
 
         else
             instance = this;
+
+        board = new Tile[rows, columns];
+
+        RectTransform boardRect = boardTransform.gameObject.GetComponent<RectTransform>();
+        RectTransform tileRect = tilePrefab.gameObject.GetComponent<RectTransform>();
+
+        boardRect.sizeDelta = new Vector2(tileRect.rect.width * columns, tileRect.rect.height * rows);
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < columns; x++)
+            {
+                board[x, y] = Instantiate(tilePrefab).GetComponent<Tile>();
+                board[x, y].transform.SetParent(boardTransform);
+
+                RectTransform rectTransform = board[x, y].GetComponent<RectTransform>();
+                rectTransform.localPosition = new Vector2(x * -rectTransform.rect.width, y * -rectTransform.rect.height);
+            }
+        }
     }
+
+
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileType
+{
+    None,
+    Straight,
+    Angle,
+    T,
+    Quad,
+}
+
 public class Tile : MonoBehaviour
 {
+    private TileType type;
+    private float tileAngle;
 
+    public void Awake()
+    {
+        type = TileType.None;
+        tileAngle = 0;
+    }
 }
